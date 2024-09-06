58fe9af755e1019f6f72782fa3788d1ce6e3f07d
Kristy-Z <kristy38rus@gmail.com>
2024-09-06 18:05:53 +0700

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,12 +1,13 @@
 def send_email(message, recipient, *, sender = 'university.help@gmail.com' ):
 
-    r = recipient.endswith('.com')  or recipient.endswith('.ru') or recipient.endswith('.net')
-    s = sender.endswith('.com') or sender.endswith('.ru') or sender.endswith('.net')
+    tuple = ('.com', '.ru', '.net')
+    r = recipient.endswith(tuple)
+    s = sender.endswith(tuple)
 
-    if '@' not in recipient and sender and r and s is False:
-        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient} ')
-    elif '@' in recipient and sender and r and s is True:
+    if '@' in recipient and '@' in sender and r is True and s is True:
         print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+    elif '@' not in recipient and '@' not in sender and r is False and s is False:
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient} ')
     elif recipient == sender:
         print("Нельзя отправить письмо самому себе!")
     else:
@@ -14,6 +15,6 @@ def send_email(message, recipient, *, sender = 'university.help@gmail.com' ):
     return
 
 send_email('Привет!', 'kristy38rus@mail.ru', sender = 'university.help@gmail.com')
-send_email('Привет!', 'kristy38rus')
+send_email('Привет!', 'kristy38rus@mail.ru')
 send_email('Привет!', 'com.kristy38rus', sender = 'university.help@gmail.com')
 send_email('Привет!', 'university.help@gmail.com', sender = 'university.help@gmail.com')
